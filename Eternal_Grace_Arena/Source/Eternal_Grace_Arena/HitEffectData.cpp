f98a267eee99443e6c19badd0abe211ef29e110d
rappidblis <schildgard@gmail.com>
2024-12-16 14:25:14 +0100

    Added HitEffectDataStruct

---
+++ Eternal_Grace_Arena/Source/Eternal_Grace_Arena/HitEffectData.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HitEffectData.h"
+
+FHitEffectData::FHitEffectData()
+{
+//	PhysicalMaterial = nullptr;
+//	NiagaraEffect = nullptr;
+//	HitSound = nullptr;
+}
---
+++ Eternal_Grace_Arena/Source/Eternal_Grace_Arena/HitEffectData.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PhysicalMaterials/PhysicalMaterial.h" // Für UPhysicalMaterial
+#include "Sound/SoundBase.h" // Für USoundBase
+#include "NiagaraSystem.h" // Für UNiagaraSystem
+#include "HitEffectData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FHitEffectData : public FTableRowBase
+{
+    GENERATED_BODY()
+
+   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hit Effect")
+   UPhysicalMaterial* PhysicalMaterial;
+ 
+   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hit Effect")
+   USoundBase* HitSound;
+ 
+   UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hit Effect")
+   UNiagaraSystem* NiagaraEffect;
+
+    // Konstruktor für Defaults
+    FHitEffectData();
+};
+
