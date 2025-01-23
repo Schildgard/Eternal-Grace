2e80ed2626fdd0aefe611a16af8321b869f29cd3
FelixGay <schildgard@gmail.com>
2025-01-23 11:57:44 +0100

    Added Hitdetect to Mushroom Kins vertical attack

--- Eternal_Grace_Arena/Intermediate/Build/Win64/x64/Eternal_Grace_ArenaEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
+++ Eternal_Grace_Arena/Intermediate/Build/Win64/x64/Eternal_Grace_ArenaEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "G:/Repository/Eternal-Grace-Arena/Eternal_Grace_Arena/Intermediate/Build/Win64/x64/Eternal_Grace_ArenaEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
+#include "D:/Leon/Eternal-Grace-Arena/Eternal_Grace_Arena/Intermediate/Build/Win64/x64/Eternal_Grace_ArenaEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
 #include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
