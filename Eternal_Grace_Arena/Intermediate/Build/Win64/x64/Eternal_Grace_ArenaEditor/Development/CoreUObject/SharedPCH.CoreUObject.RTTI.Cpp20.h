cdc5a7b0fba2caca051ee9f05a3a5bc235197d31
Schildgard <schildgard@gmail.com>
2025-01-27 22:13:25 +0100

    Second Attempt to build church. Almost done with outlining

--- Eternal_Grace_Arena/Intermediate/Build/Win64/x64/Eternal_Grace_ArenaEditor/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.h
+++ Eternal_Grace_Arena/Intermediate/Build/Win64/x64/Eternal_Grace_ArenaEditor/Development/CoreUObject/SharedPCH.CoreUObject.RTTI.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
-#include "D:/Leon Depository/Eternal-Grace-Arena/Eternal_Grace_Arena/Intermediate/Build/Win64/x64/Eternal_Grace_ArenaEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.RTTI.Cpp20.h"
+#include "G:/Repository/Eternal-Grace-Arena/Eternal_Grace_Arena/Intermediate/Build/Win64/x64/Eternal_Grace_ArenaEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.RTTI.Cpp20.h"
 #include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
--- Eternal_Grace_Arena/Intermediate/Build/Win64/x64/Eternal_Grace_ArenaEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.h
+++ Eternal_Grace_Arena/Intermediate/Build/Win64/x64/Eternal_Grace_ArenaEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "D:/Leon Depository/Eternal-Grace-Arena/Eternal_Grace_Arena/Intermediate/Build/Win64/x64/Eternal_Grace_ArenaEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.h"
+#include "G:/Repository/Eternal-Grace-Arena/Eternal_Grace_Arena/Intermediate/Build/Win64/x64/Eternal_Grace_ArenaEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.h"
 #include "Runtime/Engine/Public/EngineSharedPCH.h"
