d69bcacebbc30ed34db9394bda4c22064a6d6ddf
Schildgard <schildgard@gmail.com>
2025-01-29 19:06:54 +0100

    Updated Footstep Sounds

--- Eternal_Grace_Arena/Intermediate/Build/Win64/UnrealEditor/Inc/Eternal_Grace_Arena/UHT/Eternal_Grace_Arena.init.gen.cpp
+++ Eternal_Grace_Arena/Intermediate/Build/Win64/UnrealEditor/Inc/Eternal_Grace_Arena/UHT/Eternal_Grace_Arena.init.gen.cpp
@@ -25,13 +25,13 @@ void EmptyLinkFunctionForGeneratedCodeEternal_Grace_Arena_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x7CA4D17D,
-				0x3E1F4B05,
+				0x21C9DB9A,
+				0xE2368450,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Eternal_Grace_Arena.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_Eternal_Grace_Arena.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Eternal_Grace_Arena(Z_Construct_UPackage__Script_Eternal_Grace_Arena, TEXT("/Script/Eternal_Grace_Arena"), Z_Registration_Info_UPackage__Script_Eternal_Grace_Arena, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x7CA4D17D, 0x3E1F4B05));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Eternal_Grace_Arena(Z_Construct_UPackage__Script_Eternal_Grace_Arena, TEXT("/Script/Eternal_Grace_Arena"), Z_Registration_Info_UPackage__Script_Eternal_Grace_Arena, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x21C9DB9A, 0xE2368450));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
